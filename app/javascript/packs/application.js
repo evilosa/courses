514bdea27f2e505d230e38f5941b44fffc17def9
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-04-25 01:03:55 +0300

    Add react seed project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,11 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import routes from '../routes';
+
+ReactDOM.render(<Router routes = {routes} history={hashHistory} />, document.getElementById('root'));
