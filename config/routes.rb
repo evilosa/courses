469aa247bf5295be5166b2abaaca3f42b1536e0d
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-05-18 14:53:23 +0300

    Create client index test.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :clients, only: [:index, :create, :destroy, :update]
-      resources :courses, only: [:index, :create, :destroy, :update]
     end
   end
 
