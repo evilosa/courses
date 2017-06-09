1fc620f6c3b85ff40d4752223cf3905c072758c2
evilosa <bojkov@sorc.ru>
2017-06-09 20:15:06 +0300

    Update seed function.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times { |index| Client.create(title: "Client #{index}", tax_number: '2303002200', full_name: "Client #{index}", description: '')}
