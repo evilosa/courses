cd7f680f439b942160d46480c1b97fc6575eb782
evilosa <bojkov@sorc.ru>
2017-06-22 10:14:14 +0300

    Update gemset name.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,7 +4,7 @@ working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
 # listen
-listen "/tmp/unicorn.stackforum.sock", backlog: 1024
+listen "/tmp/unicorn.courses.sock", backlog: 1024
 
 # logging
 stderr_path "log/unicorn.stderr.log"
