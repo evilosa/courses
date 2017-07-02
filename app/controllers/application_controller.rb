b4809340baaf862b649a9424d0c13a06d897bfc9
evilosa <bojkov@sorc.ru>
2017-07-02 23:05:49 +0300

    Start create authentication process.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-    protect_from_forgery with: :null_session
 
-  before_action :configure_permitted_parameters, if: :devise_contoller?
+  protect_from_forgery with: :null_session
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :provider
-    devise_parameter_sanitizer.for(:sign_up) << :uid
-  end
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :name
+  #   devise_parameter_sanitizer.for(:sign_up) << :provider
+  #   devise_parameter_sanitizer.for(:sign_up) << :uid
+  # end
 end
