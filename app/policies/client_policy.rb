911ac183d3a6f17692c0979fcb53610c7a05e42b
evilosa <bojkov@sorc.ru>
2017-07-18 12:41:52 +0300

    Refactor client and application policy.

--- app/policies/client_policy.rb
+++ app/policies/client_policy.rb
@@ -1,25 +1,19 @@
 class ClientPolicy < ApplicationPolicy
   def index?
-    self.user.has_role?(:admin)
+    admin?
   end
 
   def show?
-    (self.user.has_role?(:user) && self.record.users.include?(self.user)) || (self.user.has_role?(:client) && self.record.users.include?(self.user)) || self.user.has_role?(:admin)
-  end
-
-  def new?
-    false
-  end
-
-  def create?
-    self.user.has_role?(:admin)
+     update? || (user? && owner?)
   end
 
   def update?
-    (self.user.has_role?(:client) && self.record.users.include?(self.user)) || self.user.has_role?(:admin)
+    admin? || (client? && owner?)
   end
 
-  def destroy?
-    self.user.has_role?(:admin)
+  private
+
+  def owner?
+    self.record.users.include?(self.user)
   end
 end
